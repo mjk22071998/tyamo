77bd5acf6b1da230cff60a23b614cf8dc0c44178
Muhammad Junaid Khalid <mjk22071998@gmail.com>
2024-03-25 09:25:23 +0500

    basic login structure ready

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -8,6 +8,51 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getAppBar(context: context),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Text("Sign in to Tyamo"),
+                SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: Image.asset(
+                    "assets/images/ic_launcher.png",
+                    filterQuality: FilterQuality.high,
+                  ),
+                )
+              ],
+            ),
+            const Text("To connect with\nyour partner"),
+            const SizedBox(
+              height: 40,
+            ),
+            TextField(
+              textAlign: TextAlign.start,
+              obscureText: false,
+              textInputAction: TextInputAction.none,
+              autofocus: false,
+              keyboardType: TextInputType.emailAddress,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                fillColor: Colors.blueGrey.shade100,
+                filled: true,
+                prefixIcon: Icon(
+                  Icons.alternate_email,
+                ),
+                prefixIconColor: MaterialStateColor.resolveWith((states) =>
+                    states.contains(MaterialState.focused)
+                        ? Colors.teal
+                        : Colors.blueGrey.shade400),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
